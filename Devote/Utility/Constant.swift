9b5e02605e29a5d1f6548e87412cf19ac686d1ed
Aleksandrs Petruhins <aleksandrs.petruhins@gmail.com>
2022-06-28 20:44:29 +0300

    Fixed core data template. Basic to-do app logic and UX

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,24 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Alex on 28/06/2022.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+
+
+// MARK: - UX
+
